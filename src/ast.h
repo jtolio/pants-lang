2d6b2696236c08cfd8d8050634b094c328021c50
JT Olds <hello@jtolds.com>
2011-01-23 01:53:03 -0700

    more parsing

--- src/ast.h
+++ src/ast.h
@@ -9,8 +9,9 @@ namespace ast {
   struct Expression { virtual ~Expression() {} protected: Expression() {} };
   struct Term { virtual ~Term() {} protected: Term() {} };
   struct Assignee { virtual ~Assignee() {} protected: Assignee() {} };
-  
+
   struct Variable : public Term {
+    Variable(const std::string& name_) : name(name_) {}
     std::string name;
   };
 
@@ -26,42 +27,53 @@ namespace ast {
     Assignee assignee;
     PTR<Expression> assignment;
   };
-  
+
   struct Definition : public Expression {
     Assignee assignee;
     PTR<Expression> assignment;
   };
-  
+
   struct List : public Expression {
+    List(const std::vector<PTR<Expression> >& values_) : values(values_) {}
     std::vector<PTR<Expression> > values;
   };
-  
+
   struct Application : public Expression {
+    Application(const std::vector<PTR<Term> >& terms_) : terms(terms_) {}
     std::vector<PTR<Term> > terms;
   };
 
   struct ListExpansion : public Term {
+    ListExpansion(const std::vector<PTR<Expression> >& expressions_)
+      : expressions(expressions_) {}
     std::vector<PTR<Expression> > expressions;
   };
-  
+
   struct SubExpression : public Term {
+    SubExpression(const std::vector<PTR<Expression> >& expressions_)
+      : expressions(expressions_) {}
     std::vector<PTR<Expression> > expressions;
   };
-  
+
   struct Call : public Term {
+    Call(const PTR<Term>& function_) : function(function_) {}
     PTR<Term> function;
   };
-  
+
   struct Lookup : public Term {
+    Lookup(const PTR<Term>& object_, const Variable& field_)
+      : object(object_), field(field_) {}
     PTR<Term> object;
     Variable field;
   };
-  
+
   struct Index : public Term {
+    Index(const PTR<Term>& record_, const std::vector<PTR<Expression> >& index_)
+      : record(record_), index(index_) {}
     PTR<Term> record;
     std::vector<PTR<Expression> > index;
   };
-  
+
   struct Function : public Term {
     std::vector<PTR<Expression> > expressions;
     Variable left_var_args;
@@ -73,27 +85,31 @@ namespace ast {
     Variable right_var_args;
     std::vector<PTR<Expression> > right_var_arg_length;
   };
-  
+
   struct Integer : public Term {
+    Integer(const long long& value_) : value(value_) {}
     long long value;
   };
-  
+
   struct CharString : public Term {
+    CharString(const std::string& value_) : value(value_) {}
     std::string value;
   };
-  
+
   struct ByteString : public Term {
+    ByteString(const std::string& value_) : value(value_) {}
     std::string value;
   };
-  
+
   struct Float : public Term {
+    Float(const double& value_) : value(value_) {}
     double value;
   };
-  
+
   struct Map : public Term {
     std::vector<std::pair<PTR<Expression>, PTR<Expression> > > values;
   };
-  
+
 }}
 
 #endif
--- src/parser.cpp
+++ src/parser.cpp
@@ -11,22 +11,27 @@ namespace parser {
   namespace ascii = boost::spirit::ascii;
   namespace phx = boost::phoenix;
   using namespace cirth::ast;
-  
+
   template <typename Iterator>
   struct grammar : qi::grammar<Iterator, std::vector<PTR<Expression> >(),
       ascii::space_type>{
     qi::rule<Iterator, std::vector<PTR<Expression> >(), ascii::space_type>
         explist;
     qi::rule<Iterator, PTR<Expression>(), ascii::space_type> expression;
-    qi::rule<Iterator, PTR<Expression>(), ascii::space_type> reassignment;
-    qi::rule<Iterator, PTR<Expression>(), ascii::space_type> definition;
+//    qi::rule<Iterator, PTR<Expression>(), ascii::space_type> reassignment;
+//    qi::rule<Iterator, PTR<Expression>(), ascii::space_type> definition;
     qi::rule<Iterator, PTR<Expression>(), ascii::space_type> list;
+    qi::rule<Iterator, std::vector<PTR<Expression> >(), ascii::space_type>
+        appcommalist;
     qi::rule<Iterator, PTR<Expression>(), ascii::space_type> application;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> term;
+    qi::rule<Iterator, std::vector<PTR<Term> >(), ascii::space_type> termlist;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> value;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> listexpansion;
+    qi::rule<Iterator, PTR<Term>(), ascii::space_type> subexpression;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> function;
-    qi::rule<Iterator, PTR<Term>(), ascii::space_type> variable;
+    qi::rule<Iterator, PTR<Term>(), ascii::space_type> termvariable;
+    qi::rule<Iterator, Variable(), ascii::space_type> variable;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> call;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> lookup;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> index;
@@ -36,17 +41,62 @@ namespace parser {
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> floating;
     qi::rule<Iterator, PTR<Term>(), ascii::space_type> map;
     qi::rule<Iterator, Assignee(), ascii::space_type> assignee;
+    qi::rule<Iterator, std::string(), ascii::space_type> identifier;
+    qi::rule<Iterator, std::string(), ascii::space_type> charstringvalue;
+    qi::rule<Iterator, std::string(), ascii::space_type> bytestringvalue;
 
     grammar() : grammar::base_type(explist) {
 
       explist = expression % ';' >> -qi::lit(';');
-
-      expression = reassignment | definition | list | application;
-      
-      reassignment = qi::lit("re")[qi::_val = phx::construct<PTR<Expression> >()];
-      definition = qi::lit("def")[qi::_val = phx::construct<PTR<Expression> >()];
-      list = qi::lit("lst")[qi::_val = phx::construct<PTR<Expression> >()];
-      application = qi::lit("app")[qi::_val = phx::construct<PTR<Expression> >()];
+      appcommalist = application >> ',' > -(application % ',');
+      termlist = +term;
+      expression = list | application; // reassignment | definition
+      list = appcommalist[
+          qi::_val = phx::construct<PTR<Expression> >(phx::new_<List>(qi::_1))];
+      application = termlist[qi::_val = phx::construct<PTR<Expression> >(
+          phx::new_<Application>(qi::_1))];
+      term = value | listexpansion;
+      value = subexpression
+              | function
+              | termvariable
+              | call
+              | lookup
+              | index
+              | integer
+              | bytestring
+              | charstring
+              | floating
+              | map;
+      listexpansion = (qi::lit("(") > explist > ")")[
+          qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<ListExpansion>(qi::_1))];
+      subexpression = (qi::lit("(") > explist > ")")[
+          qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<SubExpression>(qi::_1))];
+      identifier = qi::lexeme[qi::alpha >> *qi::alnum];
+      variable = identifier[phx::bind(&Variable::name, qi::_val) = qi::_1];
+      termvariable = identifier[qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<Variable>(qi::_1))];
+      integer = qi::long_long[qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<Integer>(qi::_1))];
+      floating = qi::double_[qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<Float>(qi::_1))];
+      call = qi::lit("call")[
+          qi::_val = phx::construct<PTR<Term> >()];
+      lookup = qi::lit("lookup")[
+          qi::_val = phx::construct<PTR<Term> >()];
+      index = qi::lit("index")[
+          qi::_val = phx::construct<PTR<Term> >()];
+      charstringvalue = qi::lexeme['"' > +(qi::char_ - '"') > '"'];
+      bytestringvalue = qi::lexeme["b\"" > +(qi::char_ - '"') > '"'];
+      charstring = charstringvalue[qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<CharString>(qi::_1))];
+      bytestring = bytestringvalue[qi::_val = phx::construct<PTR<Term> >(
+          phx::new_<ByteString>(qi::_1))];
+      map = qi::lit("map")[
+          qi::_val = phx::construct<PTR<Term> >()];
+      function = qi::lit("function")[
+          qi::_val = phx::construct<PTR<Term> >()];
 
       qi::on_error<qi::fail>(explist,
         std::cout << phx::val("Error! Expecting ") << qi::_4
@@ -69,7 +119,7 @@ int main(int argc, char** argv) {
     os << str;
   }
   str = os.str();
-  
+
   parser::grammar<std::string::const_iterator> g;
   std::vector<PTR<ast::Expression> > p;
   std::string::const_iterator iter = str.begin();
