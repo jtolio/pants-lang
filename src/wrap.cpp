f44d314557c92fb40a45d182429bd39d781fab41
JT Olds <hello@jtolds.com>
2011-02-09 23:40:30 -0700

    wrap?

--- src/wrap.cpp
+++ src/wrap.cpp
@@ -1,5 +1,40 @@
 #include "wrap.h"
 
-void cirth::wrap::wrap(std::vector<PTR<cirth::ast::Expression> >& ast) {
-  
+using namespace cirth::ast;
+
+static void bindTopLevelNames(std::vector<PTR<Expression> >&);
+static void addReturns(std::vector<PTR<Expression> >&);
+
+void cirth::wrap::wrap(std::vector<PTR<Expression> >& ast) {
+  addReturns(ast);
+  bindTopLevelNames(ast);
+}
+
+static void bindTopLevelNames(std::vector<PTR<Expression> >& ast) {
+  #define BIND_NAME(name) { \
+    std::vector<PTR<Term> > terms; \
+    terms.push_back(PTR<Term>(new FullValue(PTR<Value>(new Variable(name, \
+        false)), std::vector<PTR<Trailer> >()))); \
+    ast.insert(ast.begin(), PTR<Expression>(new Definition(PTR<Assignee>( \
+        new SingleAssignee(PTR<Term>(new FullValue(PTR<Value>(new Variable( \
+        name, true)), std::vector<PTR<Trailer> >())))), PTR<Expression>(\
+        new Application(terms))))); \
+  }
+  BIND_NAME("if");
+  BIND_NAME("while");
+  BIND_NAME("print");
+  BIND_NAME("construct");
+  BIND_NAME("import");
+  BIND_NAME("+");
+  BIND_NAME("-");
+  BIND_NAME("/");
+  BIND_NAME("*");
+  BIND_NAME("and");
+  BIND_NAME("or");
+  BIND_NAME("not");
+  #undef BIND_NAME
+}
+
+static void addReturns(std::vector<PTR<Expression> >& ast) {
+
 }
