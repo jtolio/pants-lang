16a58f085d02141871bdd4c0acba4750c03afc1b
JT Olds <hello@jtolds.com>
2011-03-27 22:29:16 -0600

    more translation, checkpoint before reworking object/array assignment

--- src/cps.cpp
+++ src/cps.cpp
@@ -14,6 +14,10 @@ std::string cps::Variable::format() const {
   return "TODO";
 }
 
+std::string cps::Function::format() const {
+  return "TODO";
+}
+
 static PTR<cps::Value> trans(const PTR<ir::Value>& val) {
   throw expectation_failure("TODO");
   return PTR<cps::Value>();
@@ -40,7 +44,55 @@ void cps::transform(const std::vector<PTR<ir::Expression> >& in_ir,
   for(unsigned int i = in_ir.size(); i > 0; --i) {
     ir::ReturnValue* rv(dynamic_cast<ir::ReturnValue*>(in_ir[i-1].get()));
     if(rv) {
+      PTR<cps::Call> call(new cps::Call);
+      out_ir = call;
+      call->function = trans(rv->term->function);
+      call->left_positional_args.reserve(rv->term->left_positional_args.size());
+      for(unsigned int i = 0; i < rv->term->left_positional_args.size(); ++i) {
+        call->left_positional_args.push_back(cps::PositionalOutArgument(trans(
+            rv->term->left_positional_args[i].variable)));
+      }
+      if(!!rv->term->left_arbitrary_arg) {
+        call->left_arbitrary_arg = cps::ArbitraryOutArgument(trans(
+            rv->term->left_arbitrary_arg.get().variable));
+      }
+      call->right_positional_args.reserve(
+          rv->term->right_positional_args.size());
+      for(unsigned int i = 0; i < rv->term->right_positional_args.size(); ++i) {
+        call->right_positional_args.push_back(cps::PositionalOutArgument(trans(
+            rv->term->right_positional_args[i].variable)));
+      }
+      call->right_optional_args.reserve(rv->term->right_optional_args.size());
+      for(unsigned int i = 0; i < rv->term->right_optional_args.size(); ++i) {
+        call->right_optional_args.push_back(cps::OptionalOutArgument(
+            rv->term->right_optional_args[i].key,
+            trans(rv->term->right_optional_args[i].variable)));
+      }
+      if(!!rv->term->right_arbitrary_arg) {
+        call->right_arbitrary_arg = cps::ArbitraryOutArgument(trans(
+            rv->term->right_arbitrary_arg.get().variable));
+      }
+      if(!!rv->term->right_keyword_arg) {
+        call->right_keyword_arg = cps::KeywordOutArgument(trans(
+            rv->term->right_keyword_arg.get().variable));
+      }
+      call->scoped_optional_args.reserve(rv->term->scoped_optional_args.size());
+      for(unsigned int i = 0; i < rv->term->scoped_optional_args.size(); ++i) {
+        call->scoped_optional_args.push_back(cps::OptionalOutArgument(
+            rv->term->scoped_optional_args[i].key,
+            trans(rv->term->scoped_optional_args[i].variable)));
+      }
+      if(!!rv->term->scoped_keyword_arg) {
+        call->scoped_keyword_arg = cps::KeywordOutArgument(trans(
+            rv->term->scoped_keyword_arg.get().variable));
+      }
+      PTR<cps::Function> continuation(new cps::Function);
+      continuation->expression = out_ir;
       throw expectation_failure("TODO");
+//      continuation->left_positional_args.push_back(cps::PositionalInArgument(
+//          rv->assignee
+//       ));
+      call->continuation = continuation;
       continue;
     }
     ir::Assignment* assignment(dynamic_cast<ir::Assignment*>(in_ir[i-1].get()));
