487307ef0fcf2907726c95c49098882d027f969d
JT Olds <hello@jtolds.com>
2011-04-17 10:15:57 -0600

    better print function

--- src/assets/start_main.c
+++ src/assets/start_main.c
@@ -20,7 +20,39 @@ int main(int argc, char **argv) {
   goto start;
 
 c_print:
-  printf("print!\n");
+  if(right_positional_args_size < 1) {
+    printf("function takes 1 arguments, %d given.\n", right_positional_args_size);
+    exit(1);
+  }
+  if(continuation.t != CLOSURE) {
+    printf("function requires continuation, none given.\n");
+    exit(1);
+  }
+  switch(right_positional_args[0].t) {
+    case INTEGER:
+      printf("%lld\n", right_positional_args[0].integer.value);
+      break;
+    case FLOAT:
+      printf("%f\n", right_positional_args[0].floating.value);
+      break;
+    case BOOLEAN:
+      if(right_positional_args[0].boolean.value) {
+        printf("true\n");
+      } else {
+        printf("false\n");
+      }
+      break;
+    case NIL:
+      printf("null\n");
+      break;
+
+    case CLOSURE:
+    case STRING:
+    case OBJECT:
+    default:
+      printf("TODO: unimplemented\n");
+      break;
+  }
   env = continuation.closure.env;
   goto *continuation.closure.func;
 
