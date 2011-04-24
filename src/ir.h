8b428e8d91e83c2c6b6c15ffeddc3c2c5e9236fb
JT Olds <hello@jtolds.com>
2011-04-24 01:15:41 -0600

    renaming 'return' to 'cont' and providing return structure for specifically designated functions

--- src/ir.h
+++ src/ir.h
@@ -11,7 +11,7 @@
 #define NULL_VALUE        ::cirth::ir::Name("null", false)
 #define HIDDEN_OBJECT     ::cirth::ir::Name("hidden_object", false)
 #define CONTINUATION      ::cirth::ir::Name("continuation", false)
-#define RETURN            ::cirth::ir::Name("return", true)
+#define RETURN            ::cirth::ir::Name("cont", true)
 #define THROW             ::cirth::ir::Name("throw", true)
 #define GENSYM_PREFIX     "ir_"
 
