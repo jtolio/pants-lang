2b7c63ab94b6e7e15f7fc43c3f45e79754006651
JT Olds <hello@jtolds.com>
2012-01-12 20:30:37 -0700

    stubbing out cps types

--- src/cps/types.py
+++ src/cps/types.py
@@ -30,4 +30,79 @@
 
 __author__ = "JT Olds"
 __author_email__ = "hello@jtolds.com"
-__all__ = []
+__all__ = ["Identifier", "Field", "Variable", "Integer", "Float", "String",
+    "OutArgument", "PositionalOutArgument", "NamedOutArgument",
+    "ArbitraryOutArgument", "KeywordOutArgument", "InArgument",
+    "RequiredInArgument", "DefaultInArgument", "ArbitraryInArgument",
+    "KeywordInArgument", "Expression", "Call", "Assignment", "ObjectMutation",
+    "Value", "Callable"]
+
+from ir import types as ir
+
+# most of this is just the same
+
+Identifier = ir.Identifier
+Field = ir.Field
+Variable = ir.Variable
+Integer = ir.Integer
+Float = ir.Float
+String = ir.String
+
+OutArgument = ir.OutArgument
+PositionalOutArgument = ir.PositionalOutArgument
+NamedOutArgument = ir.NamedOutArgument
+ArbitraryOutArgument = ir.ArbitraryOutArgument
+KeywordOutArgument = ir.KeywordOutArgument
+
+InArgument = ir.InArgument
+RequiredInArgument = ir.RequiredInArgument
+DefaultInArgument = ir.DefaultInArgument
+ArbitraryInArgument = ir.ArbitraryInArgument
+KeywordInArgument = ir.KeywordInArgument
+
+class Expression(object): pass
+
+class Call(Expression):
+  __slots__ = ["call", "left_args", "right_args", "continuation", "line", "col"]
+  def __init__(self, call, left_args, right_args, continuation, line, col):
+    self.call = call
+    self.left_args = left_args
+    self.right_args = right_args
+    self.continuation = continuation
+    self.line = line
+    self.col = col
+
+class Assignment(Expression):
+  __slots__ = ["assignee", "value", "local", "next_expression", "line", "col"]
+  def __init__(self, assignee, value, local, next_expression, line, col):
+    self.assignee = assignee
+    self.value = value
+    self.local = local
+    self.next_expression = next_expression
+    self.line = line
+    self.col = col
+
+class ObjectMutation(Expression):
+  __slots__ = ["object", "field", "value", "next_expression", "line", "col"]
+  def __init__(self, object_, field, value, next_expression, line, col):
+    self.object = object_
+    self.field = field
+    self.value = value
+    self.next_expression = next_expression
+    self.line = line
+    self.col = col
+
+class Value(object): pass
+
+class Callable(Value):
+  __slots__ = ["expression", "left_args", "right_args", "function",
+      "cont_defined", "line", "col"]
+  def __init__(self, expression, left_args, right_args, function, cont_defined,
+      line, col):
+    self.expression = expression
+    self.left_args = left_args
+    self.right_args = right_args
+    self.function = function
+    self.cont_defined = cont_defined
+    self.line = line
+    self.col = col
