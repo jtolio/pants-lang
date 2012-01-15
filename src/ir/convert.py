a54a13061bf4d69f9e6b8a789cf58e9057347c53
JT Olds <hello@jtolds.com>
2012-01-15 14:44:37 -0700

    correct copyright dates

--- src/ir/convert.py
+++ src/ir/convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2011, JT Olds <hello@jtolds.com>
+# Copyright (c) 2012, JT Olds <hello@jtolds.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
